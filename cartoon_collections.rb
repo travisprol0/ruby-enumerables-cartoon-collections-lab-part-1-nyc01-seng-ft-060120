654d9558200d0470e5c81b58729d0651996a7666
Travis Prol <travisjohnprol@gmail.com>
2020-05-18 12:27:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,16 @@
 def greet_characters(array)
+  array.each do |character|
+    puts "Hello #{character}!"
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
+    array.each_with_index do |dwarf, index|
+      puts "#{index + 1}. #{dwarf}"
+    end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
